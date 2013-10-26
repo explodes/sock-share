c1af0f06db767fdf088f8d0ebb9c46853ff16057
Evan Leis <evan.leis@wearetopsecret.com>
2013-10-25 21:32:46 -0600

    Better JS: Track paired partner, handle involuntary unpairing, hide private vars with _ prefix

--- webshare/static/js/webshare.js
+++ webshare/static/js/webshare.js
@@ -1,46 +1,62 @@
-function WebShare(host, port) {
+function WebShare(host, port, debug) {
     var self = this
         , url = 'ws://' + host + ':' + port
         ;
 
+    this.debug = debug || false;
+
     // Socket creation
-    this.socket = new WebSocket(url);
-    this.socket.onmessage = this.onmessage.bind(this);
-    this.socket.onopen = function () {
+    this._socket = new WebSocket(url);
+    this._socket.onmessage = this.onmessage.bind(this);
+    this._socket.onopen = function () {
         self.getKey(function (ok, code, body) {
             self.key = body.key;
         });
     };
 
     // Functional callbacks
-    this.callbacks = {};
+    this._callbacks = {};
 
     // Misc. callbacks
-    this.onrelay = undefined;
-    this.oncommanderror = undefined;
+    this.onrelay = undefined; // information was relayed :: f(true, 'SUCCESS', packageObject)
+    this.oncommanderror = undefined; // invalid command stuff :: f(false, 'COMMAND_ERROR', {error: message})
+    this.onunpair = undefined; // involuntary unpair :: f(success, code, null)
 
     this.key = undefined;
+    this.pairedTo = undefined;
 }
 
 WebShare.prototype.onmessage = function (msg) {
-    console.log("IN", msg.data);
+    if (this.debug) console.log(this.key, "IN", msg.data);
     var data = JSON.parse(msg.data)
         , echo = data.echo
         , ok = data.ok
         , code = data.code
         , body = data.body
         , command = data.command
-        , callbackId = echo.callbackId
+        , callbackId = echo ? echo.callbackId : null
         , callback
         ;
     if (code == 'COMMAND_ERROR') {
-        if (self.oncommanderror) self.oncommanderror(ok, code, body);
+        if (this.oncommanderror) this.oncommanderror(ok, code, body);
     } else if (command == 'relay') {
-        if (self.onrelay) self.onrelay(ok, code, body);
+        if (this.onrelay) this.onrelay(ok, code, body);
     } else if (callbackId) {
-        callback = this.callbacks[callbackId];
+        callback = this._callbacks[callbackId];
         if (callback) {
             callback(ok, code, body);
+        }
+    } else if (command == 'unpair') {
+        // involuntary unpair (unpair w/o callbackId)
+        if (this.onunpair) this.onunpair(ok, code, body);
+    }
+
+    // Track pair status
+    if (ok && code == 'SUCCESS') {
+        if (command == 'pair') {
+            this.pairedTo = body.with;
+        } else if (command == 'unpair') {
+            this.pairedTo = undefined;
         }
     }
 }
@@ -54,7 +70,7 @@ WebShare.prototype._generateCallbackId = function () {
         for (var i = 0; i < 7; i++) {
             id += possible.charAt(Math.floor(Math.random() * possible.length));
         }
-        if (!this.callbacks[id]) {
+        if (!this._callbacks[id]) {
             return id;
         }
     }
@@ -65,8 +81,9 @@ WebShare.prototype._command = function (command, body, callback) {
         , message = {echo: {callbackId: callbackId}, command: command, body: body}
         , package = JSON.stringify(message)
         ;
-    this.callbacks[callbackId] = callback;
-    this.socket.send(package);
+    this._callbacks[callbackId] = callback;
+    if (this.debug) console.log(this.key, "OUT", package);
+    this._socket.send(package);
 }
 
 WebShare.prototype.pair = function (target, callback) {
